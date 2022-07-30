f57935d95a986fd09bb854bb07020157f2efc295
hartbrkr3399 <hartbrkrlegacy@gmail.com>
2022-07-30 22:35:14 +0530

    Added first screen of Login and Signup Buttons.

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -8,16 +8,15 @@ import {
 } from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import CustomButton from './components/CustomButton';
 import {StoreProvider} from 'easy-peasy';
 import store from './Stores';
-import MainScreen from './Screens/Main';
+import LoginSignupScreen from './Screens/LoginSignup';
 
 const App: FC<unknown> = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: Colors.lighter,
   };
 
   return (
@@ -28,8 +27,7 @@ const App: FC<unknown> = () => {
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={backgroundStyle}>
-            <MainScreen />
-            <CustomButton />
+            <LoginSignupScreen />
           </ScrollView>
         </SafeAreaView>
       </PaperProvider>
---
+++ react-native/Screens/LoginSignup.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Button} from 'react-native-paper';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Dimensions} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+const viewWidth = windowWidth * 0.6;
+const viewHeight = windowHeight * 0.2;
+const colorPrimary = '#1995cd';
+const colorContrast = '#ffffff';
+
+const LoginSignupScreen = () => {
+  const handleLoginPress = () => {
+    console.log('Pressed Login.');
+  };
+
+  const handleSignupPress = () => {
+    console.log('Pressed Signup.');
+  };
+  return (
+    <View style={styles.viewParent}>
+      <View style={styles.viewChild}>
+        <Button
+          mode="contained"
+          style={styles.buttonLogin}
+          dark={true}
+          onPress={handleLoginPress}>
+          Login
+        </Button>
+        <Button
+          mode="outlined"
+          style={styles.buttonSignup}
+          color={colorPrimary}
+          onPress={handleSignupPress}>
+          Signup
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewParent: {
+    width: windowWidth,
+    height: windowHeight,
+    justifyContent: 'center',
+  },
+  viewChild: {
+    // flex: 1,
+    // backgroundColor: '#105090',
+    width: viewWidth,
+    height: viewHeight,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonLogin: {
+    position: 'relative',
+    marginVertical: 10,
+    width: '100%',
+    backgroundColor: colorPrimary,
+  },
+  buttonSignup: {
+    position: 'relative',
+    marginVertical: 10,
+    width: '100%',
+    borderColor: colorPrimary,
+    borderWidth: 1,
+  },
+});
+
+export default LoginSignupScreen;
